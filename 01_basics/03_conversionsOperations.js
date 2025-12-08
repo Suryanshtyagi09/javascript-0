98b1267af4b4cb853f456f77ce286eabe494a7e6
Suryanshtyagi09 <abeta6697@gmail.com>
2025-12-08 11:57:16 +0000

    fix: update conversion operations logic :wq

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -13,9 +13,33 @@ let valueInNumber =Number(score)
 
 let isLoggedIn = 1
 let booleanIsLoggedIn= Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 let somenumber=33
 let stringnumber=String(somenumber)
-console.log(stringnumber);
-console.log(typeof stringnumber);
+//console.log(stringnumber);
+//console.log(typeof stringnumber);
+
+let value=3
+let negvalue=-value
+console.log(negvalue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%2);
+
+let str1="hello"
+let str2="hitesh"
+
+let str3=str1 + str2
+console.log(str3)
+
+let num1,num2,num3 
+num1=num2=num3=2+2
+
+let gamecounter=100;
+++gamecounter;
+console.log(gamecounter);
