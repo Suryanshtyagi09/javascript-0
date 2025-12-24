df4ba90120f3389b04286fbd8ff2e24b7b6d0dee
Suryansh Tyagi <abeta6697@gmail.com>
2025-12-24 04:02:31 +0000

    second part of object file

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,46 @@
+// const tinderuser=new Obejct() =>singleton obejct declaration
+
+const tinderuser={}
+
+tinderuser.id="123abc"
+tinderuser.name="Surya"
+tinderuser.isLoggedIn=false
+
+//console.log(tinderuser);
+
+const regularUser={
+    email:" some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"Suryansh",
+            lastname:"Tyagi"
+        }
+    }
+}
+//console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1={1:"a", 2:"b"}
+const obj2={3:"a", 4:"b"}
+const obj3={5:"a", 6:"b"}
+
+//const obj3={obj1,obj2}  =>wrong syntax
+const obj4= Object.assign({},obj1,obj2,obj3)  //correct syntax
+
+const obj5={...obj1,...obj2} //mostly used syntax
+console.log(obj4);  
+
+const users=[
+    {
+        id:1,
+        email:"abc@gmail.com"
+    },
+    {
+
+    }
+]
+users[1].email
+
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser));
+
