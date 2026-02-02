ab4c4ddb1c3d9b4a109a845a66edd4c158a4717f
Suryansh Tyagi <abeta6697@gmail.com>
2026-02-02 11:34:55 +0000

    upadate: Added new iteration example in two.js

---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+//while loop
+
+//while(condition){
+    //code block to be executed
+//}
+let index=1
+while (index<5){
+    console.log("Hello World")
+    index=index+2
+}
+
+//do while loop
+let i = 1;
+
+do {
+  console.log(i);
+  i++;
+} while (i <= 5);
