c4bf01b2e94fb2234ee50778797d34b11baa3341
Suryansh Tyagi <abeta6697@gmail.com>
2026-03-25 10:37:01 +0000

    add:new loop concepts file

---
+++ 05_iterations/four.js
@@ -0,0 +1,17 @@
+const myObject={
+    js:'javascript',
+    cpp:'c++',
+    rb:'ruby',
+    swiftL: "swift by apple"
+} 
+for (const key in myObject){
+   // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming=["js","rb","py","cpp"]
+
+for(const key in programming){
+    console.log(programming[key]);
+    
+}
+
